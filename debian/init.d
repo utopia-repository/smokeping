046f1928d6771211c1fe8c8f2ea0f9f4ac137a02
Antoine Beaupré <anarcat@koumbit.org>
2011-11-16 11:47:42 -0500

    reindent file with spaces

--- debian/init.d
+++ debian/init.d
@@ -11,12 +11,12 @@
 # Default-Stop:      0 1 6
 # Short-Description: Start or stop the smokeping latency logging system daemon
 # Description:       SmokePing is a latency logging and graphing system
-#                    that consists of a daemon process which organizes 
-#                    the latency measurements and a CGI which presents 
+#                    that consists of a daemon process which organizes
+#                    the latency measurements and a CGI which presents
 #                    the graphs. This script is used to start or stop
 #                    the daemon.
 ### END INIT INFO
-# 
+#
 
 set -e
 
@@ -90,110 +90,110 @@ check_config () {
 
 case "$1" in
     start)
-    log_daemon_msg "Starting $DESC" $NAME
-    check_config
-    check_slave
-    set +e
-    pidofproc "$DAEMON" > /dev/null
-    STATUS=$?
-    set -e
-    if [ "$STATUS" = 0 ]
-    then
-        log_progress_msg "already running"
+        log_daemon_msg "Starting $DESC" $NAME
+        check_config
+        check_slave
+        set +e
+        pidofproc "$DAEMON" > /dev/null
+        STATUS=$?
+        set -e
+        if [ "$STATUS" = 0 ]
+        then
+            log_progress_msg "already running"
+            log_end_msg $STATUS
+            exit $STATUS
+        fi
+
+        if [ ! -d /var/run/smokeping ]; then
+            mkdir /var/run/smokeping
+            chown ${DAEMON_USER}.root /var/run/smokeping
+            chmod 0755 /var/run/smokeping
+        fi
+
+        set +e
+        start-stop-daemon --start --quiet --exec $DAEMON --oknodo \
+            --chuid $DAEMON_USER --pidfile $PIDFILE \
+            -- $DAEMON_ARGS \
+            | logger -p daemon.notice -t $NAME
+        STATUS=$?
+        set -e
+
         log_end_msg $STATUS
         exit $STATUS
-    fi
-
-    if [ ! -d /var/run/smokeping ]; then
-        mkdir /var/run/smokeping
-        chown ${DAEMON_USER}.root /var/run/smokeping
-        chmod 0755 /var/run/smokeping
-    fi
-
-    set +e
-    start-stop-daemon --start --quiet --exec $DAEMON --oknodo \
-                            --chuid $DAEMON_USER --pidfile $PIDFILE \
-                            -- $DAEMON_ARGS \
-                            | logger -p daemon.notice -t $NAME
-    STATUS=$?
-    set -e
-
-    log_end_msg $STATUS
-    exit $STATUS
-    ;;
+        ;;
 
 
     stop)
-    log_daemon_msg "Shutting down $DESC" $NAME
+        log_daemon_msg "Shutting down $DESC" $NAME
 
-    set +e
-    start-stop-daemon --oknodo --stop --retry 3 --quiet --pidfile /var/run/smokeping/$NAME.pid --signal 15
-    STATUS=$?
-    set -e
+        set +e
+        start-stop-daemon --oknodo --stop --retry 3 --quiet --pidfile /var/run/smokeping/$NAME.pid --signal 15
+        STATUS=$?
+        set -e
 
-    if [ "$STATUS" = 0 ]
-    then
-        rm -f $PIDFILE
-	pkill -u www-data speedy_backend || true
-    fi
+        if [ "$STATUS" = 0 ]
+        then
+            rm -f $PIDFILE
+            pkill -u www-data speedy_backend || true
+        fi
 
-    log_end_msg $STATUS
-    exit $STATUS
-    ;;
+        log_end_msg $STATUS
+        exit $STATUS
+        ;;
 
 
     restart)
-    # Restart service (if running) or start service
-    $0 stop
-    $0 start
-    ;;
+        # Restart service (if running) or start service
+        $0 stop
+        $0 start
+        ;;
 
 
     reload|force-reload)
-    log_action_begin_msg "Reloading $DESC configuration" 
+        log_action_begin_msg "Reloading $DESC configuration"
 
-    check_config
-    set +e
-    $DAEMON --reload $DAEMON_ARGS | logger -p daemon.notice -t smokeping
-    STATUS=$?
-    set -e
+        check_config
+        set +e
+        $DAEMON --reload $DAEMON_ARGS | logger -p daemon.notice -t smokeping
+        STATUS=$?
+        set -e
 
-    if [ "$STATUS" = 0 ]
-    then
-        log_action_end_msg 0 "If the CGI has problems reloading, see README.Debian."
-    else
-        log_action_end_msg $STATUS
-    fi
-    exit $STATUS
-    ;;
+        if [ "$STATUS" = 0 ]
+        then
+            log_action_end_msg 0 "If the CGI has problems reloading, see README.Debian."
+        else
+            log_action_end_msg $STATUS
+        fi
+        exit $STATUS
+        ;;
 
 
     status)
-    log_daemon_msg "Checking $DESC status" $NAME
-    # Use pidofproc to check the status of the service,
-    # pidofproc returns the exit status code of 0 when it the process is
-    # running.
+        log_daemon_msg "Checking $DESC status" $NAME
+        # Use pidofproc to check the status of the service,
+        # pidofproc returns the exit status code of 0 when it the process is
+        # running.
 
-    # LSB defined exit status codes for status:
-    # 0    program is running or service is OK
-    # 1    program is dead and /var/run pid file exists
-    # 2    program is dead and /var/lock lock file exists
-    # 3    program is not running
-    # 4    program or service status is unknown
-    # 5-199    reserved (5-99 LSB, 100-149 distribution, 150-199 applications)
-    
-    set +e
-    pidofproc "$DAEMON" > /dev/null
-    STATUS=$?
-    log_progress_msg "(status $STATUS)"
-    log_end_msg 0
-    set -e
-    exit $STATUS
-    ;;
+        # LSB defined exit status codes for status:
+        # 0    program is running or service is OK
+        # 1    program is dead and /var/run pid file exists
+        # 2    program is dead and /var/lock lock file exists
+        # 3    program is not running
+        # 4    program or service status is unknown
+        # 5-199    reserved (5-99 LSB, 100-149 distribution, 150-199 applications)
+
+        set +e
+        pidofproc "$DAEMON" > /dev/null
+        STATUS=$?
+        log_progress_msg "(status $STATUS)"
+        log_end_msg 0
+        set -e
+        exit $STATUS
+        ;;
 
 
     *)
-    echo "Usage: $0 {start|stop|status|restart|force-reload|reload}"
-    exit 1
-    ;;
+        echo "Usage: $0 {start|stop|status|restart|force-reload|reload}"
+        exit 1
+        ;;
 esac
